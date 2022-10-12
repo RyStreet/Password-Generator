e622a95e297d8a76162d621b7b4c1d723ae82229
Ryan Street <ryanstreet122@gmail.com>
2022-10-12 12:42:33 -0400

    added more comments to JS

--- assets/script.js
+++ assets/script.js
@@ -6,18 +6,22 @@ var userNumber;
 
 function restart() {
   userNumber = prompt("Choose password length (Between 8-128 Characters)");
+  // PW must be between 8-128 or an error message is prompted//
   if ((userNumber<8) || (userNumber>128)) {
     alert("Password must be between 8-128 characters");
    restart(); 
    }
+   // if PW length is between 8-128, run generate PW function //
    else if ((userNumber>=8) || (userNumber<=128)) {
     generatePassword(); 
    }
   
 };
 
+// generates PW//
 function generatePassword() {
   
+  //different variable strings are split into arrays//
   var num ="0123456789".split("")
   var alphalow = "abcdefghijklmnopqrstuvwxyz".split("")
   var alphahigh = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
@@ -26,6 +30,7 @@ function generatePassword() {
   var prePassword = [];
   var password = "";
 
+// various if statements determine the addition or removal or certain variables//
 if (confirm("Do you want Numbers?")) {
   prePassword = prePassword.concat(num)
 }
@@ -38,11 +43,12 @@ if (confirm("Do you want Special Characters?")) {
 if (confirm("Do you want Lower Characters?")) {
   prePassword = prePassword.concat(alphalow)
 }
-// console.log(prePassword)
+// if a user doesn't choose any variables to include, an error message appears//
 if (prePassword.length === 0){
   alert("You must choose a variable")
 }else{
 
+//for loop takes accepted variables and creates a randomized sequence based on the PW length//
 for(var i=0; i< userNumber; i++){
   var randomNumber = Math.floor(Math.random()*prePassword.length);
  
